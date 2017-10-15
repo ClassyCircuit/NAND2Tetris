dce9ad063705db92a1e8824cb503b4a4f0fc24f4
janisims <simsonsjanis@gmail.com>
2017-10-15 17:37:51 +0300

    wrote multiplier in assembly

--- 04.Machine_Language/mult/mult.asm
+++ 04.Machine_Language/mult/mult.asm
@@ -6,4 +6,51 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//=====	PSEUDO CODE =====
+//	a=R0		//3
+//	b=R1		//4
+//	sum=a		//3		
+//	for(i=1; i<=b; i++){
+//		sum += a;
+//		i += 1
+//	}
+	@R0
+	D=M
+	@a
+	M=D		//init a
+
+	@sum
+	M=D		//init sum
+	
+	@R1
+	D=M
+	@b
+	M=D		//init b
+	
+	@i
+	M=1		//init i counter to keep track of iterations
+(LOOP)
+	@b
+	D=M
+	@i
+	D=D-M
+	@STOP
+	D;JLE	//if b-i <= 0, then jump to STOP
+
+	@a
+	D=M
+	@sum
+	M=M+D	//add a to sum
+
+	@i
+	M=M+1	//increment i by 1
+	@LOOP
+	0;JMP
+(STOP)
+	@sum
+	D=M
+	@R2
+	M=D		//set RAM[2] to value of sum
+(END)
+	@END
+	0;JMP
